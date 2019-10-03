1cfbd625df5546310d3a2d6507e9295113c397ae
Ross Malcolm <rossmalc75@gmail.com>
2019-10-03 00:24:28 -0400

    Added Seasons

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay'
 
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {lat: null, errorMessage: ''};
+    state = {lat: null, errorMessage: ''};
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({errorMessage: err.message}); 
-            }
+            position => this.setState({lat: position.coords.latitude }),
+            err => this.setState({errorMessage: err.message}) 
         );
     }
 
@@ -29,11 +24,7 @@ class App extends React.Component{
         }
 
         if(!this.state.errorMessage && this.state.lat){
-            return(
-            <div>
-                Latitude: {this.state.lat}
-            </div>
-            );    
+            return <SeasonDisplay lat = {this.state.lat}/>;
         }
 
         if (!this.state.errorMessage && !this.state.lat){
